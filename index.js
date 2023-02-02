f28184e6401bf32ea6aea0d01fdd7885504cd35c
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-01 20:58:00 -0700

    complete all tests

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  if (amount === "generous") {
+    return "Thank you so much.";
+  } else if (amount === "not as generous"){
+    return "Thank you.";
+  } else if (amount !== "generous" && amount !== "not as generous") {
+    return "Bye."
+  }
 }
